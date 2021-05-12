09dcfe03734b099f689b09cbb66ac557c289ef6b
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-12 19:36:01 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -119,8 +119,10 @@ for (size_t i=0;i< m1._nfil;i+=1){
 
 Registro1= _mm_set1_epi32 (m1._matrixInMemory[0]);
 Registro2= _mm_loadu_si64(&m1._matrixInMemory[0]);
+__m128i result =_mm_cmpgt_epi32(Registro1,Registro2);
 mostrar_registro(Registro1);
 mostrar_registro(Registro2);
+mostrar_registro(result);
 
 
 
