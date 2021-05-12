dcd67abd7c18942fd6e3ce1145112af1c01e5be7
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-11 23:21:41 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -66,12 +66,12 @@ int main(int argc, char** argv)
 
 	__m128i Registro1,Registro2;
 	std::cout <<  m1._matrixInMemory << std::endl;
-	for (size_t i=0;i<m1._matrixInMemory.lenght();i+=8){
+	//for (size_t i=0;i<m1._matrixInMemory.lenght();i+=8){
 		//Registro1= _mm_load_si128(&m1._matrixInMemory[i]);
 		//Registro2= _mm_load_si128(&m1._matrixInMemory[i+4]);
 		//std::cout <<  Registro1 << std::endl;
 		//std::cout <<  Registro2 << std::endl;
-	}
+	//}
 
 
 
