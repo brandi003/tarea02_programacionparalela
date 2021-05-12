fb19cc6aea60f3d7486cb9bc5e3d47f883cac693
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-12 16:29:02 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -73,18 +73,11 @@ __m128i Registro1,Registro2;
 
 //}
 
-
+Registro1 =_mm_move_epi64(Registro1);
 uint32_t *vectorOut1 = (uint32_t*)aligned_alloc (32, 8);
 _mm_storeu_si64(vectorOut1,Registro1);
 std::cout <<  vectorOut1[0] << std::endl;
 std::cout <<  vectorOut1[1] << std::endl;
-
-
-Registro1 =_mm_move_epi64(Registro1);
-uint32_t *vectorOut2 = (uint32_t*)aligned_alloc (32, 8);
-_mm_storeu_si64(vectorOut2,Registro1);
-std::cout <<  vectorOut2[0] << std::endl;
-std::cout <<  vectorOut2[1] << std::endl;
 
 	return(EXIT_SUCCESS);
 }
