ed7dc1e4c634119fef772a192e19720f4b2e309d
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-12 17:39:49 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -93,8 +93,8 @@ for (size_t i=0;i< m1._nfil;i+=2){
 		}
 		if(!(int)vectorOut1[1]==0){
 			auto aux=m1._matrixInMemory[i];
-			m1._matrixInMemory[i]=m1._matrixInMemory[j];
-			m1._matrixInMemory[j]=aux;
+			m1._matrixInMemory[i+1]=m1._matrixInMemory[j+1];
+			m1._matrixInMemory[j+1]=aux;
 			Registro1= _mm_loadu_si64(&m1._matrixInMemory[i]);
 			Registro2= _mm_loadu_si64(&m1._matrixInMemory[j]);
 		}
