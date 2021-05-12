128026a93ea53b880e24855356b9bfe03ffa1b39
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-12 17:01:09 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -82,7 +82,7 @@ _mm_storeu_si64(vectorOut2,Registro2);
 std::cout <<  vectorOut1[0] << "--" << vectorOut2[0] << std::endl;
 std::cout <<  vectorOut1[1] << "--" << vectorOut2[1] << std::endl;
 __m128i result =_mm_cmpgt_epi64(Registro1,Registro2);
-_mm_storeu_si64(vectorOut1,Registro1);
+_mm_storeu_si64(vectorOut1,result);
 std::cout << "---------------" <<std::endl;
 std::cout << vectorOut1[0] <<std::endl;
 std::cout << vectorOut1[1] <<std::endl;
