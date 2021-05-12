286c973439dd29e4318f974baa8fd8de7b278048
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-12 17:52:32 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -76,7 +76,7 @@ __m128i Registro1,Registro2;
 
 }*/
 std::cout << m1._nfil << std::endl;
-for (size_t i=0;i< m1._nfil;i+=1){
+for (size_t i=0;i< m1._nfil;i+=2){
 	Registro1= _mm_loadu_si64(&m1._matrixInMemory[i]);
 	for (size_t j=0;j<m1._nfil;j+=2){
 		Registro2= _mm_loadu_si64(&m1._matrixInMemory[j]);
@@ -89,14 +89,14 @@ for (size_t i=0;i< m1._nfil;i+=1){
 			m1._matrixInMemory[j]=aux;
 			Registro1= _mm_loadu_si64(&m1._matrixInMemory[i]);
 			Registro2= _mm_loadu_si64(&m1._matrixInMemory[j]);
-		}/*
+		}
 		if((int)vectorOut1[1]==0){
-			auto aux=m1._matrixInMemory[i];
+			auto aux=m1._matrixInMemory[i+1];
 			m1._matrixInMemory[i+1]=m1._matrixInMemory[j+1];
 			m1._matrixInMemory[j+1]=aux;
 			Registro1= _mm_loadu_si64(&m1._matrixInMemory[i]);
 			Registro2= _mm_loadu_si64(&m1._matrixInMemory[j]);
-		}*/
+		}
 	}
 }
 for(size_t i=0; i< 5; i++){		
