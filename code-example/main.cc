3334b8bb6ad256a4f4850c05853bd69851277471
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-11 23:21:02 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -65,7 +65,7 @@ int main(int argc, char** argv)
 	//aquiempieza mi basura
 
 	__m128i Registro1,Registro2;
-	
+	std::cout <<  m1._matrixInMemory << std::endl;
 	for (size_t i=0;i<m1._matrixInMemory.lenght();i+=8){
 		//Registro1= _mm_load_si128(&m1._matrixInMemory[i]);
 		//Registro2= _mm_load_si128(&m1._matrixInMemory[i+4]);
