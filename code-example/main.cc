c35946a54d7d6c983700f5187dc11e81db16d91a
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-12 16:07:13 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -68,7 +68,7 @@ int main(int argc, char** argv)
 std::cout << "tamaño arreglo:  " << m1._nfil << std::endl;
 __m128i Registro1,Registro2;
 //for (size_t i=0;i< m1._nfil;i+=2){
-	Registro1= _mm_loadu_si64(&m1._matrixInMemory[0]);
+	Registro1= _mm_loadu_si16(&m1._matrixInMemory[0]);
 	//Registro2= _mm_loadu_si64(&m1._matrixInMemory[0]);
 
 //}
@@ -80,7 +80,8 @@ std::cout << "tamaño del vectorOut1: " << sizeof(vectorOut1) << std::endl;
 _mm_storeu_si64(vectorOut1,Registro1);
 //uint32_t *vectorOut2 = (uint32_t*)aligned_alloc (32, sizeof(uint32_t)*2);
 //_mm_storeu_si64(vectorOut2,Registro2);
-std::cout << "tamaño del vectorOut1: " << sizeof(double) << std::endl;
+std::cout << "tamaño del vectorOut1: " << sizeof(uint32_t) << std::endl;
+
 std::cout <<  vectorOut1[0] << std::endl;
 std::cout <<  vectorOut1[1] << std::endl;
 std::cout <<  vectorOut1[2] << std::endl;
