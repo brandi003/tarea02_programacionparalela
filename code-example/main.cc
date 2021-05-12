329d88b8f336df70f96bb0a1a7649384447b36b7
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-12 09:07:19 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -67,13 +67,11 @@ int main(int argc, char** argv)
 std::cout <<  m1._nfil << std::endl;
 	__m128i Registro1,Registro2;
 	for (size_t i=0;i< m1._nfil;i+=8){
-		auto prueba = &m1._matrixInMemory[i];
-
-		Registro1= _mm_loadu_si64(prueba);
+		Registro1= _mm_loadu_si64(i);
 		//Registro2= _mm_load_si128(&m1._matrixInMemory[i+4]);
 
-		std::cout <<  &m1._matrixInMemory[i] << std::endl;
-		std::cout <<  &m1._matrixInMemory[i+4] << std::endl;
+		//std::cout <<  &m1._matrixInMemory[i] << std::endl;
+		std::cout <<  Registro1 << std::endl;
 	}
 
 
