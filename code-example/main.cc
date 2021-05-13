61005dab03e9865dbee73f832cecaee2a5559ff7
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-13 11:44:33 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -120,7 +120,7 @@ for (size_t i=0;i< m1._nfil;i+=1){
 		__m128i result =_mm_sub_epi32(Registro1,Registro2);
 		uint32_t *vectorOut1 = (uint32_t*)aligned_alloc (64, 8);
 		_mm_storeu_si64(vectorOut1,result);
-		if((int)vectorOut1[0]>=0 && (int)vectorOut1[1]>=0){
+		/*if((int)vectorOut1[0]>=0 && (int)vectorOut1[1]>=0){
 			continue;
 		}else if((int)vectorOut1[0]<0 && (int)vectorOut1[1]>0){
 			auto aux=m1._matrixInMemory[i];
@@ -142,19 +142,22 @@ for (size_t i=0;i< m1._nfil;i+=1){
 			m1._matrixInMemory[i]=m1._matrixInMemory[j+1];
 			m1._matrixInMemory[j+1]=aux;
 			Registro1= _mm_set1_epi32 (m1._matrixInMemory[i]);
-		}
+		}*/
 		
 	}
 	
 }
 
+uint32_t *vectorOut1 = (uint32_t*)aligned_alloc (8, 8);
+Registro2= _mm_loadu_si64(&m1._matrixInMemory[999]);
+_mm_storeu_si64(vectorOut1,Registro2);
 
-
-
+std::cout <<  vectorOut1[0] << std::endl;
+std::cout <<  vectorOut1[1] << std::endl;
 
 
 for(size_t i=0; i< m1._nfil; i++){		
-		std::cout <<  m1._matrixInMemory[i] << std::endl;
+		//std::cout <<  m1._matrixInMemory[i] << std::endl;
 	}
 std::cout << "-------------------------------"<< std::endl;
 
