05e7dc929193893e3e2fe10d96b34042f8fcd89f
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-13 10:12:45 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -114,7 +114,7 @@ for (size_t i=0;i< m1._nfil;i+=1){
 	}
 }*/
 for (size_t i=0;i< m1._nfil;i+=4){
-	Registro1= _mm_setr_epi32(m1._matrixInMemory[i],m1._matrixInMemory[i+1],m1._matrixInMemory[i+2],m1._matrixInMemory[i+3]);
+	Registro1= _mm_set_epi32(m1._matrixInMemory[i],m1._matrixInMemory[i+1],m1._matrixInMemory[i+2],m1._matrixInMemory[i+3]);
 	uint32_t *vectorOut1 = (uint32_t*)aligned_alloc (64, 8);
 	_mm_storeu_si64(vectorOut1,Registro1);
 
@@ -122,7 +122,7 @@ for (size_t i=0;i< m1._nfil;i+=4){
 Registro1= _mm_setr_epi32(3,5,2,80);
 uint32_t vectorOut1[4];
 //_mm_storeu_si64(vectorOut1,Registro1);
-std::cout <<  *Registro1 << std::endl;
+std::cout <<  Registro1 << std::endl;
 std::cout <<  (uint32_t)Registro1[0] << std::endl;
 std::cout <<  (uint32_t)Registro1[1] << std::endl;
 std::cout <<  (uint32_t)Registro1[2] << std::endl;
