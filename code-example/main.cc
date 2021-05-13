b5d071f6b44c3aab23fd6966a474a939edeccf66
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-13 10:47:41 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -120,7 +120,19 @@ for (size_t i=0;i< m1._nfil;i+=1){
 		__m128i result =_mm_sub_epi32(Registro1,Registro2);
 		uint32_t *vectorOut1 = (uint32_t*)aligned_alloc (64, 8);
 		_mm_storeu_si64(vectorOut1,result);
-		if((int)vectorOut1[0]<(int)vectorOut1[1]){
+		if((int)vectorOut1[0]>0 && (int)vectorOut1[1]>0){
+			continue;
+		}else if((int)vectorOut1[0]<0 && (int)vectorOut1[1]>0){
+			auto aux=m1._matrixInMemory[i];
+			m1._matrixInMemory[i]=m1._matrixInMemory[j];
+			m1._matrixInMemory[j]=aux;
+			Registro1= _mm_set1_epi32 (m1._matrixInMemory[i]);
+		}else if((int)vectorOut1[0]>0 && (int)vectorOut1[1]<0){
+			auto aux=m1._matrixInMemory[i];
+			m1._matrixInMemory[i]=m1._matrixInMemory[j+1];
+			m1._matrixInMemory[j+1]=aux;
+			Registro1= _mm_set1_epi32 (m1._matrixInMemory[i]);
+		}else if((int)vectorOut1[0]<(int)vectorOut1[1]){
 			auto aux=m1._matrixInMemory[i];
 			m1._matrixInMemory[i]=m1._matrixInMemory[j];
 			m1._matrixInMemory[j]=aux;
