6d7cafc84bab42732067befe42139649b7d09f2d
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-13 12:21:32 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -23,18 +23,6 @@ void uso(std::string pname)
 	exit(EXIT_FAILURE);
 }
 
-void mostrar_registro(__m128i registro){
-	uint32_t *vectorOut1 = (uint32_t*)aligned_alloc (64, 128);
-	_mm_storeu_si64(vectorOut1,registro);
-	std::cout << "---------------" <<std::endl;
-	std::cout << vectorOut1[0] <<std::endl;
-	std::cout << vectorOut1[1] <<std::endl;
-	std::cout << vectorOut1[2] <<std::endl;
-	std::cout << vectorOut1[3] <<std::endl;
-	std::cout << "---------------" <<std::endl;
-
-}
-
 int main(int argc, char** argv)
 {
 
@@ -63,7 +51,7 @@ int main(int argc, char** argv)
 	std::cout << "Time to transfer to main memory: " << timer0.elapsed() << std::endl;
 	
 	timer1.start();
-	//std::sort(m1._matrixInMemory, m1._matrixInMemory + m1._nfil);
+	std::sort(m1._matrixInMemory, m1._matrixInMemory + m1._nfil);
 	timer1.stop();
 	
 	std::cout << "Time to sort in main memory: " << timer1.elapsed() << std::endl;
@@ -75,44 +63,69 @@ int main(int argc, char** argv)
 	}
 	std::cout << "-------------------------------"<< std::endl;
 
-	
-	
-//////////////////////
-	//aquiempieza mi basura
-std::cout << "tamaño arreglo:  " << m1._nfil << std::endl;
-__m128i Registro1,Registro2;
+/////////////////////
+	// mierda original que se va a quedar al final
 
-
-/*
-std::cout << m1._nfil << std::endl;
-for (size_t i=0;i< m1._nfil;i+=1){
-	Registro1= _mm_set1_epi32 (m1._matrixInMemory[i]);
-	for (size_t j=0;j<m1._nfil;j+=2){
-		Registro2= _mm_loadu_si64(&m1._matrixInMemory[j]);
-		__m128i result =_mm_cmpgt_epi32(Registro1,Registro2);
-		uint32_t *vectorOut1 = (uint32_t*)aligned_alloc (64, 8);
-		_mm_storeu_si64(vectorOut1,result);
-		if(!(int)vectorOut1[0]==0){
-			auto aux=m1._matrixInMemory[i];
-			m1._matrixInMemory[i]=m1._matrixInMemory[j];
-			m1._matrixInMemory[j]=aux;
-			Registro1= _mm_set1_epi32 (m1._matrixInMemory[i]);
-			Registro2= _mm_loadu_si64(&m1._matrixInMemory[j]);
-		}
-		if(!(int)vectorOut1[0]==0 && !(int)vectorOut1[1]==0){
-			j=j-2;
-			continue;
-		}
-		if(!(int)vectorOut1[1]==0){
-			auto aux=m1._matrixInMemory[i];
-			m1._matrixInMemory[i]=m1._matrixInMemory[j+1];
-			m1._matrixInMemory[j+1]=aux;
-			Registro1= _mm_set1_epi32 (m1._matrixInMemory[i]);
-			Registro2= _mm_loadu_si64(&m1._matrixInMemory[j]);
+	Timing timer2, timer3;
+	////////////////////////////////////////////////////////////////
+	// Transferir la matriz del archivo fileName a memoria principal
+	timer2.start();
+	MatrixToMem m2(fileName);
+	timer2.stop();
+	
+	std::cout << "Time to transfer to main memory: " << timer2.elapsed() << std::endl;
+	
+	timer3.start();
+	__m128i Registro1,Registro2;
+	for (size_t i=0;i< m2._nfil;i+=1){
+		Registro1= _mm_set1_epi32 (m2._matrixInMemory[i]);
+		for (size_t j=i;j<m2._nfil;j+=2){
+			Registro2= _mm_loadu_si64(&m2._matrixInMemory[j]);
+			__m128i result =_mm_sub_epi64(Registro1,Registro2);
+			uint32_t *vectorOut1 = (uint32_t*)aligned_alloc (64, 8);
+			_mm_storeu_si64(vectorOut1,result);
+			if((int)vectorOut1[0]>=0 && (int)vectorOut1[1]>=0){
+				continue;
+			}else if((int)vectorOut1[0]<0 && (int)vectorOut1[1]>0){
+				auto aux=m2._matrixInMemory[i];
+				m2._matrixInMemory[i]=m2._matrixInMemory[j];
+				m2._matrixInMemory[j]=aux;
+				Registro1= _mm_set1_epi32 (m2._matrixInMemory[i]);
+			}else if((int)vectorOut1[0]>0 && (int)vectorOut1[1]<0){
+				auto aux=m2._matrixInMemory[i];
+				m2._matrixInMemory[i]=m2._matrixInMemory[j+1];
+				m2._matrixInMemory[j+1]=aux;
+				Registro1= _mm_set1_epi32 (m2._matrixInMemory[i]);
+			}else if((int)vectorOut1[0]<=(int)vectorOut1[1]){
+				auto aux=m2._matrixInMemory[i];
+				m2._matrixInMemory[i]=m2._matrixInMemory[j];
+				m2._matrixInMemory[j]=aux;
+				Registro1= _mm_set1_epi32 (m2._matrixInMemory[i]);
+			}else if((int)vectorOut1[0]>(int)vectorOut1[1]){
+				auto aux=m2._matrixInMemory[i];
+				m2._matrixInMemory[i]=m2._matrixInMemory[j+1];
+				m2._matrixInMemory[j+1]=aux;
+				Registro1= _mm_set1_epi32 (m2._matrixInMemory[i]);
+			}
+			
 		}
 		
 	}
-}*/
+	timer3.stop();
+	
+	std::cout << "Time to sort in main memory: " << timer3.elapsed() << std::endl;
+	
+	////////////////////////////////////////////////////////////////
+	// Mostrar los 5 primeros elementos de la matriz ordenada.
+	for(size_t i=0; i< 5; i++){		
+		std::cout <<  m1._matrixInMemory[i] << std::endl;
+	}
+	std::cout << "-------------------------------"<< std::endl;
+	
+//////////////////////
+	//aquiempieza mi basura
+
+__m128i Registro1,Registro2;
 for (size_t i=0;i< m1._nfil;i+=1){
 	Registro1= _mm_set1_epi32 (m1._matrixInMemory[i]);
 	for (size_t j=i;j<m1._nfil;j+=2){
@@ -147,6 +160,7 @@ for (size_t i=0;i< m1._nfil;i+=1){
 	}
 	
 }
+
 /*
 uint32_t *vectorOut1 = (uint32_t*)aligned_alloc (8, 8);
 Registro2= _mm_loadu_si64(&m1._matrixInMemory[999]);
