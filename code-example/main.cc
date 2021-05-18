3fcf32f5d14546a0d6e68d549a3d33c001880ddd
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-18 00:59:20 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -116,21 +116,21 @@ void print_matriz(__m128i* Registros){
 int shellSort(uint32_t* matriz, int n)
 {
     // Start with a big gap, then reduce the gap
-    for (int gap = n/2; gap > 0; gap /= 2)
+    for (uint32_t gap = n/2; gap > 0; gap /= 2)
     {
         // Do a gapped insertion sort for this gap size.
         // The first gap elements a[0..gap-1] are already in gapped order
         // keep adding one more element until the entire array is
         // gap sorted
-        for (int i = gap; i < n; i += 1)
+        for (uint32_t i = gap; i < n; i += 1)
         {
             // add a[i] to the elements that have been gap sorted
             // save a[i] in temp and make a hole at position i
-            int temp = matriz._matrixInMemory[i];
+            uint32_t temp = matriz._matrixInMemory[i];
  
             // shift earlier gap-sorted elements up until the correct
             // location for a[i] is found
-            int j;           
+            uint32_t j;           
             for (j = i; j >= gap && matriz._matrixInMemory[j - gap] > temp; j -= gap)
                 matriz._matrixInMemory[j] = matriz._matrixInMemory[j - gap];
              
