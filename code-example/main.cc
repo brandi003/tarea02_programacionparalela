8620efd33dd75c7db41a509732f9014799420290
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-18 01:32:58 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -228,7 +228,7 @@ int main(int argc, char** argv)
 	for (size_t k = 0;k < m2._nfil;k++){
 		bool flag=false;
 		int var=0;
-		for (size_t i=k+var; i<16 ; i+=16){
+		for (size_t i=k+var; i<m2._nfil ; i+=16){
 			if(m2._matrixInMemory[k]>m2._matrixInMemory[i]){
 				uint32_t aux=m2._matrixInMemory[k];
 				m2._matrixInMemory[k]=m2._matrixInMemory[i];
