985fc5f22af5e2859d5095bb15c89d1577683b03
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-20 22:15:15 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -158,7 +158,7 @@ int shellSort(MatrixToMem m2, int n)
 
 int main(int argc, char** argv)
 {
-	ofstream file;
+	std::ofstream file;
 	file.open("arrayData-10000000.txt");
 	file << "10000000\n";
 	for (size_t i=0; i<9999999; i++){
