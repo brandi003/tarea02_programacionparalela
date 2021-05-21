97a79e0587f33b03d077c1bbb87fb2c8555baf89
brandi003 <brandon.diaz@alumnos.uv.cl>
2021-05-20 21:21:41 -0400

    Update main.cc

--- code-example/main.cc
+++ code-example/main.cc
@@ -211,7 +211,7 @@ int main(int argc, char** argv)
 		__m128i Registros[4];
 		for (size_t i=0;i<m2._nfil;i+=16){
 			if(m2._nfil==1000 && i==992){
-				continue;
+				break;
 			}
 			Registros[0]=_mm_setr_epi32(m2._matrixInMemory[i],m2._matrixInMemory[i+1],m2._matrixInMemory[i+2],m2._matrixInMemory[i+3]);
 			Registros[1]=_mm_setr_epi32(m2._matrixInMemory[i+4],m2._matrixInMemory[i+5],m2._matrixInMemory[i+6],m2._matrixInMemory[i+7]);
